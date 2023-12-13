7739d49d36c36ac07caa32188d10590ba8001729
Oleksii Kravchuk <oleksij@webmaestro.com.ua>
2023-12-13 11:27:45 +0200

    curl client service provider

--- src/CurlClientServiceProvider.php
+++ src/CurlClientServiceProvider.php
@@ -12,12 +12,18 @@ class CurlClientServiceProvider extends ServiceProvider
             __DIR__ . '/../config/curl_client.php',
             'curl_client'
         );
+
+        $this->app->bind(CurlClient::class, function ($app) {
+            return new CurlClient($app['config']->get('curl_client.script_path'));
+        });
     }
 
     public function boot(): void
     {
-        $this->publishes([
-            __DIR__.'/../config/curl_client.php' => config_path('curl_client.php'),
-        ]);
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/curl_client.php' => config_path('curl_client.php'),
+            ], 'curl-client-config');
+        }
     }
 }
